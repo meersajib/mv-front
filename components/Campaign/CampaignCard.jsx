6a213d885fef3d795b68c3667d90566ae55c137b
Mamun Bhuiyan <mamunbhuiyan919@gmail.com>
2021-12-25 22:47:12 +0600

    update

--- components/Campaign/CampaignCard.jsx
+++ components/Campaign/CampaignCard.jsx
@@ -1,18 +1,21 @@
 import { Button } from "reactstrap";
 
 const ProductCard = ({ item, onClick }) => {
-
+		
+	const { title, description, price, image } = item;
 	return (
 		<div className="card h-100">
 			<img
-				src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-				className="card-img-top" alt="..." />
+				src={image}
+				className="card-img-top" alt={title} />
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				<h5 className="card-title">{title}</h5>
+				<p className="card-text">{description}</p>
 			</div>
-			<div className="card-footer">
-				<small className="text-muted">Last updated 3 mins ago</small>
+			<div className="card-footer p-0">
+				<div class="d-grid">
+					<button class="btn btn-outline-dark" type="button">ORDER NOW</button>
+				</div>
 			</div>
 		</div>
 	);
